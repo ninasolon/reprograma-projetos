3f60a110cafa2753502d817a6f5769e4980d594e
ninasolon <ninasolon@gmail.com>
2019-04-30 17:07:48 -0300

    js e css projeto 2

--- p2-to-do-list/script.js
+++ p2-to-do-list/script.js
@@ -7,16 +7,37 @@ let listaTarefas = document.getElementById("listaTarefas");
 
 adicionarTarefa.addEventListener("click", function(){
 
-    let tarefaNova = document.getElementById("tarefaNova").value; 
+    let novaTarefa = document.getElementById("novaTarefa"); 
 
-    if (tarefaNova !== "") {
-        // console.log(tarefaNova)
-
+    if (novaTarefa.value !== "") {
+        
+        //criar linha para cada tarefa
         let itemLista = document.createElement("li");
-
-        itemLista.innerHTML = `<input type="checkbox"><label>${tarefaNova}</label>`;
-
+        itemLista.innerHTML = 
+        `<div>
+            <input type="checkbox" class="check">
+            <label>${novaTarefa.value}</label>
+        </div>
+        <button onclick="excluirTarefa(this)" class="btn-excluir">x</button>`;
+        // itemLista.classList = "lista";
         listaTarefas.appendChild(itemLista);
+        
+        //voltando para default do input
+        novaTarefa.value = "";
+       
+        //mostrando botões da lista
+        let botoesLista = document.getElementById("botoesLista");
+        botoesLista.classList.remove("botoes-lista")
+
+        //botão excluir todas as tarefas
+        let excluirTodas = document.getElementById("excluirTodas");
+        excluirTodas.addEventListener("click", function(){
+
+            listaTarefas.removeChild(itemLista);
+            botoesLista.classList.add("botoes-lista"); // só funciona com classList.remove no evento anterior
+            // botoesLista.classList.toggle("botoes-lista-estilo")
+        });
+
     }
 
     else {
@@ -24,3 +45,19 @@ adicionarTarefa.addEventListener("click", function(){
     }
 
 });
+
+//botão selecionar todas as tarefas
+let selecionarTodas = document.getElementById("selecionarTodas");
+selecionarTodas.addEventListener("click", function(){
+    let check = document.querySelectorAll(".check")
+    check.forEach(function(item){
+        item.checked = true;
+    })
+
+});
+
+function excluirTarefa(elemento){
+   elemento.parentElement.remove();
+    
+}
+
